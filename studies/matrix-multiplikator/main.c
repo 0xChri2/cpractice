2dce7b0b9b5b1139a76812d3ff65c4637a894ab6
ImChri2 <chris@chris-industries.de>
2022-10-31 12:06:33 +0100

    fixed bug in matrix-multiplkatior

--- studies/matrix-multiplikator/main.c
+++ studies/matrix-multiplikator/main.c
@@ -13,7 +13,11 @@ int main() {
         {4, 5, 6},
         {7, 8, 9}
     };
-    int matrix_c [SIZE][SIZE];
+    int matrix_c [SIZE][SIZE] = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
 
     for(int i = 0;i < SIZE;i++) {
         for (int j = 0; j < SIZE; j++) {
