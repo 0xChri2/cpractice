186968c892f814ddece30552c4be3c245e9c682e
ImChri2 <chris@chris-industries.de>
2022-11-07 15:03:21 +0100

    finished asciigraphic

---
+++ studies/asciigraphic/cmake-build-debug/CMakeFiles/asciigraphic.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/asciigraphic.dir/main.c.o"
+  "CMakeFiles/asciigraphic.dir/main.c.o.d"
+  "asciigraphic"
+  "asciigraphic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/asciigraphic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- studies/asciigraphic/main.c
+++ studies/asciigraphic/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /* screen size - adapt to console size */
 #define NUM_COLUMNS 80
@@ -30,7 +31,7 @@ void update_screen()
 /* checks whether a given point (x,y) is within the screen */
 unsigned in_screen(int x, int y) {
     // TODO
-    if(screen[x][y] > screen[NUM_COLUMNS][NUM_ROWS])
+    if(x >= 0 && x < NUM_COLUMNS && y >= 0 && y < NUM_ROWS)
     {
 
         return 1;
@@ -45,30 +46,35 @@ unsigned in_screen(int x, int y) {
 /* draw a single point at given position (x,y) and with given character */
 void draw_pixel(int x, int y, char c) {
     // TODO
-    putchar(screen[x][y] = c);
+    if(in_screen(x,y)) {
+        screen[x][y] = c;
+    }
 }
 
 /* clear a single pixel at position (x,y) */
 void clear_pixel(int x, int y) {
     // TODO
-    putchar(screen[x][y] = ' ');
+    if(in_screen(x,y))
+    {
+        screen[x][y] = ' ';
+    }
 }
 
 /* draw a horizontal line (x1,y)-(x2,y) with dashes (-) */
 void draw_horizontal_line(int x1, int x2, int y) {
     // TODO
-    //if(x1 < x2){
+    if(x1 < x2){
         for(int i = x1; i <=x2;i++)
         {
-            putchar(screen[i][y] = '-');
+            screen[i][y] = '-';
         }
-   // }
-   // else{
-      //  for(int i = x1; i >=x2;i++)
-    //    {
-    //        putchar(screen[i][y] = '-');
-    //    }
-   // }
+    }
+    else{
+        for(int i = x2; i <=x1;i++)
+       {
+            screen[i][y] = '-';
+       }
+    }
 }
 
 /* draw a vertical line (x,y1)-(x,y2) with vertical bars (|) */
@@ -76,8 +82,7 @@ void draw_vertical_line(int x, int y1, int y2) {
     // TODO
     for(int i = y1; i <=y2;i++)
     {
-        putchar(screen[x][i] = '|');
-        putchar('\n');
+        screen[x][i] = '|';
     }
 }
 
@@ -102,27 +107,57 @@ void draw_vertical_arrow(int x, int y1, int y2) {
 /* draw a rectangle (x1,y1)-(x2,y1)-(x2,y2)-(x1,y2) with plus, dashes and vertical bars */
 void draw_rectangle(int x1, int y1, int x2, int y2) {
     // TODO
+    draw_horizontal_line(x1, x2, y1);
+    draw_horizontal_line(x1, x2, y2);
+    draw_vertical_line(x1, y1, y2);
+    draw_vertical_line(x2, y1, y2);
+    draw_pixel(x1,y1, '+');
+    draw_pixel(x1,y2, '+');
+    draw_pixel(x2,y1, '+');
+    draw_pixel(x2,y2, '+');
 }
 
 void draw_filled_rectangle(int x1, int y1, int x2, int y2, char c) {
     // TODO
+    for(int i = x1 + 1; i < x2; i++)
+    {
+        for(int j = y1 + 1; j < y2;j++)
+        {
+            screen[i][j] = c;
+        }
+    }
+    draw_rectangle(x1,y1,x2,y2);
 }
 
 /* draw a general line (x1,y1)-(x2,y2) with character c */
 void draw_line(int x1, int y1, int x2, int y2, char c) {
     // TODO
-    for(int i = x1; i <= x2; i++)
-    {
-        for(int j = y1; j <= y2;j++)
-        {
-            putchar(screen[i][j] = c);
-        }
+    int dx = abs(x2-x1), sx = x1<x2 ? 1 : -1;
+    int dy = abs(y2-y1), sy = y1<y2 ? 1 : -1;
+    int err = (dx>dy ? dx : -dy)/2, e2;
+
+    for(;;){
+        draw_pixel(x1,y1, c);
+        if (x1==x2 && y1==y2) break;
+        e2 = err;
+        if (e2 >-dx) { err -= dy; x1 += sx; }
+        if (e2 < dy) { err += dx; y1 += sy; }
     }
 }
 
 /* flood fill an area starting at given position with character c */
 void flood_fill(int x, int y, char c) {
     // TODO
+    if(in_screen(x, y)) {
+        if(screen[x][y] == ' ') {
+            draw_pixel(x, y, c);
+            flood_fill(x+1, y, c);
+            flood_fill(x-1, y, c);
+            flood_fill(x, y+1, c);
+            flood_fill(x, y-1, c);
+        }
+    }
+
 }
 
 
