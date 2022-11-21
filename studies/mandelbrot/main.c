cf48efeabb55d32b25f8ec8a7f1b21bc41fd5dd6
ImChri2 <chris@chris-industries.de>
2022-11-21 15:54:19 +0100

    updated mandelbrot

--- studies/mandelbrot/main.c
+++ studies/mandelbrot/main.c
@@ -15,7 +15,6 @@ const complex_t complex_1 = {1,0};
 /** constant i (often called j in electrical engineering) */
 const complex_t complex_j = {0,1};
 
-//dont know if const is right right here
 void complex_print(const complex_t *c ) {
     printf("%lf + %lf*j\n", c->real, c->imag );
 }
@@ -66,8 +65,8 @@ void complex_sub( complex_t *c0, complex_t *c1, complex_t *c2 ) {
  */
 void complex_mul( complex_t *c0, complex_t *c1, complex_t *c2 ) {
     // TODO
-    c0->real = c1->real * c2->real;
-    c0->imag = c1->imag * c2->imag;
+    c0->real = c1->real * c2->real - c1->imag * c2->imag;
+    c0->imag = c1->real * c2->imag + c1->imag * c2->real;
 }
 
 
@@ -79,8 +78,14 @@ void complex_mul( complex_t *c0, complex_t *c1, complex_t *c2 ) {
  */
 void complex_div( complex_t *c0, complex_t *c1, complex_t *c2 ) {
     // TODO
-    c0->real = c1->real / c2->real;
-    c0->imag = c1->imag / c2->imag;
+    if (c2->real == 0) {
+        printf("Re(c) = 0; Division by 0 undefined!\n");
+    } else if (c2->imag == 0) {
+        printf("Im(c) = 0; Division by 0 undefined!\n");
+    } else {
+        c0->real = (c1->real * c2->real + c1->imag * c2->imag) / (c2->real * c2->real + c2->imag * c2->imag);
+        c0->imag = (c1->imag * c2->real - c1->real * c2->imag) / (c2->real * c2->real + c2->imag * c2->imag);
+    }
 }
 
 
@@ -91,6 +96,8 @@ void complex_div( complex_t *c0, complex_t *c1, complex_t *c2 ) {
  */
 void complex_reciprocal( complex_t *c0, complex_t *c1 ) {
     // TODO
+    c0->real = c1->real / (c1->real * c1->real + c1->imag * c1->imag);
+    c0->imag = -1 * c1->imag / (c1->real * c1->real + c1->imag * c1->imag);
 }
 
 
@@ -101,6 +108,8 @@ void complex_reciprocal( complex_t *c0, complex_t *c1 ) {
  */
 void complex_conjugate( complex_t *c0, complex_t *c1 ) {
     // TODO
+    c0->real = c1->real;
+    c0->imag = c1->imag * (-1);
 }
 
 
@@ -111,10 +120,15 @@ void complex_conjugate( complex_t *c0, complex_t *c1 ) {
  */
 char grayscale( double x ) {
     const char map[] = " .:-=+*#%@";
-    if( x > 0.0) {
-        return 'O';
-    } else
-        return ' ';
+    if(x < 0.0) {
+        return map[0];
+    }
+    else if(x > 1.0) {
+        return map[9];
+    }
+    else {
+        return map[(int) (x * 10.0)];
+    }
 }
 
 
@@ -165,15 +179,65 @@ void mandelbrot_on_stack() {
 
 }
 
+int zoom(int *zahl)
+{
+    if(zahl == 0)
+    {
+        return 0;
+    }
+    else if(zahl == 3)
+    {
+        double imag1 = 0;
+        double imag2 = 1.5;
+        double imag3 = 0.05;
+        double real1 = -2;
+        double real2 = -0.5;
+        double real3 = 0.015;
+
+    }
+    else {
+        for (double imag = 0; imag <= 1.5; imag += 0.05) {
+            for (double real = -2; real <= -0.5; real += 0.015) {
+                complex_t z = complex_0;
+                complex_t z2 = complex_0;
+                complex_t c = {real, imag};
+                double gray = 0.0;
+                for (int i = 0; i < 16; i++) {
+                    complex_mul(&z2, &z, &z);
+                    complex_add(&z, &z2, &c);
+                    if (complex_abs(&z) > 2.0) {
+                        gray = i / 16.0;
+                        break;
+                    }
+                }
+                printf("%c", grayscale(gray));
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
+
 /* main function */
 
 int main () {
 
-    // testing complex numbers
+    //testing complex numbers
     complex_number_test01();
 
     // TODO: uncomment after implementing complex numbers
-    // mandelbrot_on_stack();
+    mandelbrot_on_stack();
 
+    int eingabe;
+    printf("\n Geben sie eine Zahl zwischen 0 und 5 ein: ");
+    scanf(" %d", &eingabe);
+    int *zahl;
+    zahl = &eingabe;
+    if(*zahl == 0){
+        return 0;
+    }
+    else {
+        zoom(*zahl);
+    }
     return 0;
 }
