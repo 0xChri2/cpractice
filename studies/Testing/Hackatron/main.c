dc4850098ae787a5adaf4804bf42bbfe80484484
ImChri2 <chris@chris-industries.de>
2023-02-04 21:52:00 +0100

    updated Hackatron
    added array element

--- studies/Testing/Hackatron/main.c
+++ studies/Testing/Hackatron/main.c
@@ -7,17 +7,39 @@ int main() {
         char Name[20];
     } Team;
    typedef struct {
-       Team Team ;
+
        int jurypoints;
        int pubpoints;
+   }points;
+   typedef struct {
+       Team Team ;
+       points points;
    }Hackatron;
 
-    Hackatron Team1;
-    Team1.jurypoints = 10;
-    Team1.pubpoints = 8;
-    strcpy(Team1.Team.Name,"Peter");
-    sprintf(Team1.Team.sign,"Nix");
+    Hackatron competition[10];
+    competition[0].points.jurypoints = 10;
+    competition[0].points.pubpoints = 8;
+    strcpy(competition[0].Team.Name,"Peter");
+    sprintf(competition[0].Team.sign,"Nix");
+    printf("Name = %s, Sign: %s, Points: %d/%d",competition[0].Team.Name,competition[0].Team.sign,competition[0].points.jurypoints,competition[0].points.pubpoints);
 
-    printf("Name = %s, Sign: %s, Points: %d/%d",Team1.Team.Name,Team1.Team.sign,Team1.jurypoints,Team1.pubpoints);
+    char sign[20];
+    char Name[20];
+    int jurypoints =0;
+    int pubpoints= 0;
+    printf("Geben Sie Ihren Zeichen ein:");
+    scanf("%s",&sign);
+    printf("Geben Sie Ihren Namen ein:");
+    scanf("%s",&Name);
+    printf("Geben Sie Ihren Jurypoints ein:");
+    scanf("%d",&jurypoints);
+    printf("Geben Sie Ihren Pubpoints ein:");
+    scanf("%d",&pubpoints);
+    strcpy(competition[1].Team.sign, sign);
+    strcpy(competition[1].Team.Name, Name);
+    competition[1].points.jurypoints = jurypoints;
+    competition[1].points.pubpoints = pubpoints;
+    printf("Name = %s, Sign: %s, Points: %d/%d",competition[1].Team.Name,competition[1].Team.sign,competition[1].points.jurypoints,competition[1].points.pubpoints);
 
+    return 0;
 }
