af760053ae6a782f05761dbcf00aad634624870f
ImChri2 <chris@chris-industries.de>
2023-03-28 14:11:59 +0200

    added subtraction to fraction

--- studies/s2/fractions/bruch.c
+++ studies/s2/fractions/bruch.c
@@ -8,6 +8,12 @@ bruch_t summe ( bruch_t * values , int n ) {
         r = add (r , values [ i ]);
     return r;
 }
+bruch_t subsum ( bruch_t * values , int n ) {
+    bruch_t r = values [0];
+    for (int i = 1; i < n; i ++)
+        r = sub(r , values [ i ]);
+    return r;
+}
 
 void btos ( bruch_t q , char *s , int n) {
     snprintf (s , n , " (% d /% d )" , q .z , q .n );
@@ -30,12 +36,21 @@ static void reduce ( bruch_t * q) { // warum ein Zeiger ??
 }
 
 bruch_t add ( bruch_t a , bruch_t b) {
-    int z = a .z * b.n + b. z * a .n;
-    int n = a .n * b.n ;
+    int z = a.z * b.n + b.z * a.n;
+    int n = a.n * b.n ;
     bruch_t r = {z , n };
     reduce (& r ); // call by reference !!!
     return r;
 }
+
+bruch_t sub ( bruch_t a , bruch_t b) {
+    int z = a.z * b.n - b.z * a.n;
+    int n = a.n * b.n ;
+    bruch_t r = {z , n };
+    reduce (& r ); // call by reference !!!
+    return r;
+}
+
 void output ( bruch_t q ) {
     printf (" (% d /% d )\ n " , q.z , q. n );
 }
--- studies/s2/fractions/main.c
+++ studies/s2/fractions/main.c
@@ -4,8 +4,12 @@
 
 int main ( void ) {
 // ein Array von f¨u nf Br ¨u chen :
+/*addieren
     bruch_t werte [5] = {{1 ,2} , {1 ,3} , {1 ,4} , {1 ,5} , {1 ,6}};
     bruch_t result = summe(werte , 5); // Array als Parameter
+
+
+
 
     char s [32] , str [5][32]; // Zeichenketten
     for (int i = 0; i < 5; i ++)
@@ -13,5 +17,16 @@ int main ( void ) {
     btos(result ,s ,32);
 
     printf ("% s + % s + % s + %s + %s = %s \n" , str [0] , str [1] ,str [2] , str [3] , str [4] , s );
+*/
+//subtahieren
+    bruch_t subtra [2] = {{5 ,4} , {2 ,6}};
+    bruch_t result = subsum(subtra, 2);
+
+    char s[32], str[5][32];
+    for(int i = 0;i < 2;i++)
+        btos(subtra[i], str[i],32);
+    btos(result,s,32);
+
+    printf ("% s - % s = %s \n" , str [0] , str [1] , s );
     return 0;
 }
