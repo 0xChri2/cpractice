ee5d79ee25904225ce7fe98d702c489b7c857b3d
ImChri2 <chris@chris-industries.de>
2023-04-04 09:42:30 +0200

    added heap_t

---
+++ studies/s2/heap_t/heap.c
@@ -0,0 +1,37 @@
+//
+// Created by Christoph Riedel on 03.04.23.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include "heap.h"
+
+heap_t *createHeap(){
+    heap_t *h = malloc(sizeof(heap_t));
+    h->data = (int) malloc(sizeof(int) *10);
+    h->size = 10;
+    h->min = 1;
+}
+
+char insert(heap_t *h, int val){
+    h->data = val;
+}
+
+int minimum(heap_t *h){
+    return h->min;
+}
+
+char extractMin(heap_t *h){
+
+}
+
+char getError(heap_t *h){
+    return h->error;
+}
+
+char* toString(heap_t *h){
+
+}
+
+void destroyHeap(heap_t *h){
+    free(h);
+}
---
+++ studies/s2/heap_t/heap.h
@@ -0,0 +1,22 @@
+//
+// Created by Christoph Riedel on 03.04.23.
+//
+
+#ifndef HEAP_T_HEAP_H
+#define HEAP_T_HEAP_H
+typedef struct{
+    int data;
+    int error;
+    int size;
+    int min;
+}heap_t;
+
+heap_t *createHeap();
+char insert(heap_t *h, int val);
+int minimum(heap_t *h);
+char extractMin(heap_t *h);
+char getError(heap_t *h);
+char* toString(heap_t *h);
+void destroyHeap(heap_t *h);
+
+#endif //HEAP_T_HEAP_H
