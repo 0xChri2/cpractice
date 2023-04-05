680e163ece17082ae72fd25c9471e598a4172403
ImChri2 <chris@chris-industries.de>
2023-04-05 11:14:46 +0200

    update time added getcurrenttime function

--- studies/s2/time/main.c
+++ studies/s2/time/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <time.h>
+
+char* getcurrenttime(void){
+    time_t now;
+    time(&now);
+    struct tm *local_time = localtime(&now);
+    static char buf[80];
+    strftime(buf, sizeof(buf), "%d.%m.%Y", local_time);
+    return buf;
+};
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    char* time;
+    time = getcurrenttime();
+    printf("Time: %s", time);
 }
+
+
