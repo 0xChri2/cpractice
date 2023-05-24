3e64c13cb747a8da4f2f0fc7f3f2f8e79b4b4738
ImChri2 <chris@chris-industries.de>
2023-05-24 11:45:15 +0200

    changed salary calc on worker

--- studies/s2/worker/main.cpp
+++ studies/s2/worker/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+#include <ctime>
 
 using namespace std;
+
+class date {
+private:
+    int _tag;
+    int _monat;
+    int _jahr;
+
+public:
+    date(int tag, int monat, int jahr) : _tag(tag), _monat(monat), _jahr(jahr) {}
+
+    int diff(date& other) {
+        // Unterschied in Jahren berechnen
+        int jahrdiff = other._jahr - _jahr;
+
+        // Überprüfen, ob der Geburtstag im aktuellen Jahr bereits stattgefunden hat
+        if (other._monat < _monat || (other._monat == _monat && other._tag < _tag)) {
+            jahrdiff--;
+        }
+
+        return jahrdiff;
+    }
+};
 
 class worker{
 protected:
@@ -38,11 +62,27 @@ public:
 };
 
 class chef : public worker{
+private:
+    date _birthdate{1,1,1900};
 public:
-    chef(int id, string name, int basesalary) : worker(id, name, basesalary){};
+    chef(int id, string name, int basesalary, date birthdate ) : worker(id, name, basesalary) {
+        _birthdate = birthdate;
+    }
     ~chef(){}
-    int salary(){
-        return _basesalary;
+    int salary() {
+        auto currentdate1 = chrono::system_clock::now();
+        time_t currentdate_time = chrono::system_clock::to_time_t(currentdate1);
+        tm* currentdate_tm = localtime(&currentdate_time);
+        date currentdate(currentdate_tm->tm_mday, currentdate_tm->tm_mon + 1, currentdate_tm->tm_year + 1900);
+        int age = _birthdate.diff(currentdate);
+
+        int additionalSalary = (age > 23) ? (age - 23) * 50 : 0;
+
+        return _basesalary + additionalSalary;
+    }
+
+    int getAge(date& currentdate) {
+        return _birthdate.diff(currentdate);
     }
 };
 
@@ -55,7 +95,7 @@ int main() {
     cout << "Manager ID: " << mgr1.getID() << endl;
     cout << "Salary: " << mgr1.salary() << endl;
 
-    chef chf1(3, "Mike Johnson", 6000);
+    chef chf1(3, "Mike Johnson", 3000, date(10, 2, 1997));
     cout << "Chef ID: " << chf1.getID() << endl;
     cout << "Salary: " << chf1.salary() << endl;
 
