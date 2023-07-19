54b78f0d277928ec63d82fa31512d4c5c1070bbf
ImChri2 <chris@chris-industries.de>
2023-07-19 21:19:41 +0200

    finished tokenizer in s2

--- studies/s2/tokenizer/main.cpp
+++ studies/s2/tokenizer/main.cpp
@@ -4,40 +4,48 @@
 
 using namespace std;
 
-class token{
+class token {
 private:
     string _input;
-    char _delimiter;
-
+    string _delimiter;
+    size_t pos;
 public:
-    token(string input, char delimiter){
-        _delimiter = delimiter;
-        _input = input;
-    }
-    ~token(){
+    token(const std::string& input, string delim)
+            : _input(input), _delimiter(delim), pos(0) {}
 
-    }
-    string strtotoken(){
+    ~token() {}
 
-        for(int i = 0; i<= _input.length();i++){
-            if(_input[i] == _delimiter){
+    string getNextToken() {
+        // Sucht das nächste Vorkommen des Trennzeichens ab der aktuellen Position
+        size_t start = _input.find_first_not_of(_delimiter, pos);
+        size_t end = _input.find_first_of(_delimiter, start);
 
-            }
+        // Überprüft, ob das Trennzeichen gefunden wurde
+        if (end == std::string::npos) {
+            // Wenn kein weiteres Trennzeichen gefunden wurde, gibt den Rest des Strings zurück
+            string token = _input.substr(start);
+            // Setzt die Position auf das Ende des Strings, um den Abbruch der Schleife zu signalisieren
+            pos = _input.length();
+            return token;
         }
-    };
+
+        // Aktualisiert die Position für den nächsten Aufruf
+        pos = end + 1;
+
+        // Gibt das gefundene Token zurück
+        return _input.substr(start, end - start);
+    }
 };
 
-
-
 int main() {
-    char input[] = "Hello, World! This is a test.";
-    const char* delimiter = " ,.!";  // Trennzeichen: Leerzeichen, Komma, Punkt, Ausrufezeichen
+    std::string input = "Hello, World! This is a sample string.";
+    string delimiter = " ";
 
-    char* token = strtok(input, delimiter);
+    token tkn(input, delimiter);
 
-    while (token != NULL) {
+    std::string token;
+    while ((token = tkn.getNextToken()) != "") {
         std::cout << "Token: " << token << std::endl;
-        token = strtok(NULL, delimiter);
     }
 
     return 0;
