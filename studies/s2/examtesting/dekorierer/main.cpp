c83ec8cbf904a2def61cd2a3f613bc002f2c7f6c
ImChri2 <chris@chris-industries.de>
2023-07-20 21:26:32 +0200

    first running verison of dekorierer in examtesting in s2

--- studies/s2/examtesting/dekorierer/main.cpp
+++ studies/s2/examtesting/dekorierer/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
+using namespace std;
+
+class Getränk {
+public:
+    virtual int preis() = 0;
+};
+class kaffee : public Getränk{
+public:
+    int preis() override{
+        return 2;
+    };
+};
+class dekorierer : public Getränk{
+protected:
+    Getränk* Drink;
+public:
+    dekorierer(Getränk* Drink) : Drink(Drink){};
+        int preis() override {
+                return Drink->preis() +2;
+        }
+};
+
+class test : public dekorierer{
+public:
+    test(Getränk* Drink) : dekorierer(Drink) {};
+    int preis()override{
+        return Drink->preis() +2;
+    };
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Getränk* kaffeen = new kaffee();
+    kaffeen = new dekorierer(kaffeen);
+    kaffeen = new test(kaffeen);
+    cout << "Preis: " << kaffeen->preis();
     return 0;
 }
