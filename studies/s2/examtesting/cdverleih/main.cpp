87bc3dba301d2baee26418f38e843582eab47541
ImChri2 <chris@chris-industries.de>
2023-07-20 14:58:50 +0200

    finished cdshop in examtesting in s2

--- studies/s2/examtesting/cdverleih/main.cpp
+++ studies/s2/examtesting/cdverleih/main.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
 
 using namespace std;
+class Kategorie{
+protected:
+    int _basispreis;
+    int _leihdauer;
+    int _strafgebuehr;
+public:
+    Kategorie(int basispreis, int leihdauer, int strafgebuehr): _basispreis(basispreis), _leihdauer(leihdauer), _strafgebuehr(strafgebuehr){};
+    virtual int preis(int tage) = 0;
+};
+
+class normal : public Kategorie{
+public:
+    normal(int basispreis, int leihdauer, int strafgebuehr): Kategorie(basispreis, leihdauer, strafgebuehr){};
+    int preis(int tage) override{
+        return _basispreis * tage;
+    }
+};
+
+class Bestseller : public Kategorie{
+public:
+    Bestseller(int basispreis, int leihdauer, int strafgebuehr): Kategorie(basispreis, leihdauer, strafgebuehr){};
+    int preis(int tage) override{
+        return _basispreis * tage;
+    };
+};
+
+class Angebot : public Kategorie{
+public:
+    Angebot(int basispreis, int leihdauer, int strafgebuehr): Kategorie(basispreis, leihdauer, strafgebuehr){};
+    int preis(int tage) override{
+        if(_leihdauer <= tage){
+            return _basispreis * _leihdauer;
+        } else {
+            return _strafgebuehr * tage;
+        }
+    }
+};
 
 int main() {
-    
+    Kategorie* m[3] = {
+            new normal(10, 10, 1000), new Bestseller(20,20,2000), new Angebot(30, 30 ,3000)
+    };
+    cout << "M1: " << m[0]->preis(10) << endl;
+    cout << "M2: " << m[1]->preis(10) << endl;
+    cout << "M3: " << m[2]->preis(10) << endl;
     return 0;
 }
